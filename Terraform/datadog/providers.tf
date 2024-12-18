7d0f9cbc833ec6ec999305bebdb46590b08f4e70
Marcus <marcus.v.silva@live.com>
2024-12-18 18:46:27 -0300

    primeira infra com terraform no datadog

--- Terraform/datadog/providers.tf
+++ Terraform/datadog/providers.tf
@@ -5,3 +5,9 @@ terraform {
     }
   }
 }
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = var.api_url
+}
---
+++ Terraform/datadog/variables.tf
@@ -0,0 +1,11 @@
+variable "datadog_api_key" {
+    default = "e371f788d5441779ac78bbd307588750"
+}
+
+variable "datadog_app_key" {
+    default = "e8812ef98880da0ebbd2cdf8860500fab53770ac"
+}
+
+variable "api_url" {
+    default = "https://us5.datadoghq.com/"
+}
